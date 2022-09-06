80cb57d2969cb234f95e52b660e1f924b47a46ad
Mark Lavercombe <mlava@users.noreply.github.com>
2022-09-06 19:03:02 +1000

    Add Featured and On This Day imports

--- extension.js
+++ extension.js
@@ -147,7 +147,7 @@ export default {
         extensionAPI.settings.panel.create(config);
 
         window.roamAlphaAPI.ui.commandPalette.addCommand({
-            label: "Wikipedia import",
+            label: "Wikipedia Page Import",
             callback: () => {
                 const uid = window.roamAlphaAPI.ui.getFocusedBlock()?.["block-uid"];
                 fetchWiki(uid).then(async (blocks) => {
@@ -160,11 +160,49 @@ export default {
                 });
             },
         });
+        window.roamAlphaAPI.ui.commandPalette.addCommand({
+            label: "Wikipedia On This Day",
+            callback: () => {
+                const uid = window.roamAlphaAPI.ui.getFocusedBlock()?.["block-uid"];
+                fetchOTD().then(async (blocks) => {
+                    const parentUid = uid || await window.roamAlphaAPI.ui.mainWindow.getOpenPageOrBlockUid();
+                    blocks.forEach((node, order) => createBlock({
+                        parentUid,
+                        order,
+                        node
+                    }))
+                })
+            }
+        });
+        window.roamAlphaAPI.ui.commandPalette.addCommand({
+            label: "Wikipedia Featured Content",
+            callback: () => {
+                const uid = window.roamAlphaAPI.ui.getFocusedBlock()?.["block-uid"];
+                fetchWFC().then(async (blocks) => {
+                    const parentUid = uid || await window.roamAlphaAPI.ui.mainWindow.getOpenPageOrBlockUid();
+                    blocks.forEach((node, order) => createBlock({
+                        parentUid,
+                        order,
+                        node
+                    }))
+                })
+            }
+        });
 
         const args = {
             text: "WIKIPEDIA",
             help: "Import data from Wikipedia",
             handler: (context) => fetchWiki,
+        };
+        const args1 = {
+            text: "ONTHISDAY",
+            help: "Import notable historical events, international holidays, and births and deaths of notable people from Wikipedia",
+            handler: (context) => fetchOTD,
+        };
+        const args2 = {
+            text: "WIKIFEATURED",
+            help: "Import today's featured content from Wikipedia",
+            handler: (context) => fetchWFC,
         };
 
         if (window.roamjs?.extension?.smartblocks) {
@@ -175,6 +213,27 @@ export default {
                 () =>
                     window.roamjs?.extension.smartblocks &&
                     window.roamjs.extension.smartblocks.registerCommand(args)
+            );
+        }
+        if (window.roamjs?.extension?.smartblocks) {
+            window.roamjs.extension.smartblocks.registerCommand(args1);
+        } else {
+            document.body.addEventListener(
+                `roamjs:smartblocks:loaded`,
+                () =>
+                    window.roamjs?.extension.smartblocks &&
+                    window.roamjs.extension.smartblocks.registerCommand(args1)
+            );
+        }
+
+        if (window.roamjs?.extension?.smartblocks) {
+            window.roamjs.extension.smartblocks.registerCommand(args2);
+        } else {
+            document.body.addEventListener(
+                `roamjs:smartblocks:loaded`,
+                () =>
+                    window.roamjs?.extension.smartblocks &&
+                    window.roamjs.extension.smartblocks.registerCommand(args2)
             );
         }
 
@@ -241,8 +300,8 @@ export default {
                                     {
                                         text: "**Wikipedia Summary:** #rm-hide #rm-horizontal",
                                         children: [
-                                            { text: ""+results[0].value.string+"" },
-                                            { text: ""+results[1].value+"" },
+                                            { text: "" + results[0].value.string + "" },
+                                            { text: "" + results[1].value + "" },
                                         ]
                                     },
                                     {
@@ -258,10 +317,18 @@ export default {
     },
     onunload: () => {
         window.roamAlphaAPI.ui.commandPalette.removeCommand({
-            label: 'Wikipedia import'
+            label: 'Wikipedia Page Import'
+        });
+        window.roamAlphaAPI.ui.commandPalette.removeCommand({
+            label: 'Wikipedia On This Day'
+        });
+        window.roamAlphaAPI.ui.commandPalette.removeCommand({
+            label: 'Wikipedia Featured Content'
         });
         if (window.roamjs?.extension?.smartblocks) {
             window.roamjs.extension.smartblocks.unregisterCommand("WIKIPEDIA");
+            window.roamjs.extension.smartblocks.unregisterCommand("ONTHISDAY");
+            window.roamjs.extension.smartblocks.unregisterCommand("WIKIFEATURED");
         };
     }
 }
@@ -271,3 +338,83 @@ function sendConfigAlert(key) {
         alert("Please enter an integer for extract length in the configuration settings via the Roam Depot tab.");
     }
 }
+
+async function fetchOTD() {
+    let today = new Date();
+    let month = today.getMonth() + 1;
+    let day = today.getDate();
+    let url = `https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/all/${month}/${day}`;
+
+    let response = await fetch(url);
+    if (response.ok) {
+        let data = await response.json();
+        function shuffle(array) {
+            var i = array.length,
+                j = 0,
+                temp;
+            while (i--) {
+                j = Math.floor(Math.random() * (i + 1));
+                temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+            return array;
+        }
+        var ranNums = shuffle(data.selected);
+
+        let string = "in ";
+        string += ranNums[0].year;
+        string += ":\n\n";
+        string += ranNums[0].text;
+        string += "";
+        let string1 = "in ";
+        string1 += ranNums[1].year;
+        string1 += ":\n\n";
+        string1 += ranNums[1].text;
+        string1 += "";
+        let string2 = "in ";
+        string2 += ranNums[2].year;
+        string2 += ":\n\n";
+        string2 += ranNums[2].text;
+        string2 += "";
+        return [
+            {
+                text: "**On this Day...** #rm-hide #rm-horizontal",
+                children: [
+                    {text: string},
+                    {text: string1},
+                    {text: string2},
+                ]
+            }
+        ];
+    } else {
+        console.error(data);
+    }
+};
+
+async function fetchWFC() {
+    let today = new Date();
+    let year = today.getFullYear();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    let url = `https://api.wikimedia.org/feed/v1/wikipedia/en/featured/${year}/${mm}/${dd}`;
+
+    let response = await fetch(url);
+    let data = await response.json();
+    if (response.ok) {
+        return [
+            {
+                text: "**Featured Article: [[" + data.tfa.titles.normalized + "]]** #rm-hide #rm-horizontal",
+                children: [
+                    { text: "" + data.tfa.extract + "" },
+                    { text: "!["+data.tfa.titles.normalized+"](" + data.tfa.originalimage.source + ")" },
+                ]
+            },
+            {
+                text: "" + data.tfa.content_urls.desktop.page + ""
+            },
+        ];
+    } else {
+        console.error(data);
+    }
+};

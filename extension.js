6f4e05b598fe4a1f3363417317a1f98b12321cf1
Mark Lavercombe <mlava@users.noreply.github.com>
2023-04-14 08:10:47 +1000

    Add files via upload

--- extension.js
+++ extension.js
@@ -1,135 +1,3 @@
-// copied and adapted from https://github.com/dvargas92495/roamjs-components/blob/main/src/writes/createBlock.ts
-const createBlock = (params) => {
-    const uid = window.roamAlphaAPI.util.generateUID();
-    return Promise.all([
-        window.roamAlphaAPI.createBlock({
-            location: {
-                "parent-uid": params.parentUid,
-                order: params.order,
-            },
-            block: {
-                uid,
-                string: params.node.text
-            }
-        })
-    ].concat((params.node.children || []).map((node, order) =>
-        createBlock({ parentUid: uid, order, node })
-    )))
-};
-
-// copied and adapted from https://github.com/dvargas92495/roamjs-components/blob/main/src/components/FormDialog.tsx
-const FormDialog = ({
-    onSubmit,
-    title,
-    options,
-    question,
-    onClose,
-}) => {
-    const [data, setData] = window.React.useState(options[0].id);
-    const onClick = window.React.useCallback(
-        () => {
-            onSubmit(data);
-            onClose();
-        },
-        [data, onClose]
-    );
-    const onCancel = window.React.useCallback(
-        () => {
-            onSubmit("");
-            onClose();
-        },
-        [onClose]
-    )
-    return window.React.createElement(
-        window.Blueprint.Core.Dialog,
-        { isOpen: true, onClose: onCancel, title, },
-        window.React.createElement(
-            "div",
-            { className: window.Blueprint.Core.Classes.DIALOG_BODY },
-            question,
-            window.React.createElement(
-                window.Blueprint.Core.Label,
-                {},
-                "Wikipedia Entries:",
-                window.React.createElement(
-                    window.Blueprint.Select.Select,
-                    {
-                        activeItem: data,
-                        onItemSelect: (id) => setData(id),
-                        items: options.map(opt => opt.id),
-                        itemRenderer: (item, { modifiers, handleClick }) => window.React.createElement(
-                            window.Blueprint.Core.MenuItem,
-                            {
-                                key: item,
-                                text: options.find(opt => opt.id === item).label,
-                                active: modifiers.active,
-                                onClick: handleClick,
-                            }
-                        ),
-                        filterable: false,
-                        popoverProps: {
-                            minimal: true,
-                            captureDismiss: true,
-                        }
-                    },
-                    window.React.createElement(
-                        window.Blueprint.Core.Button,
-                        {
-                            text: options.find(opt => opt.id === data).label,
-                            rightIcon: "double-caret-vertical"
-                        }
-                    )
-                )
-            )
-        ),
-        window.React.createElement(
-            "div",
-            { className: window.Blueprint.Core.Classes.DIALOG_FOOTER },
-            window.React.createElement(
-                "div",
-                { className: window.Blueprint.Core.Classes.DIALOG_FOOTER_ACTIONS },
-                window.React.createElement(
-                    window.Blueprint.Core.Button,
-                    { text: "Cancel", onClick: onCancel, }
-                ),
-                window.React.createElement(
-                    window.Blueprint.Core.Button,
-                    { text: "Submit", intent: "primary", onClick }
-                )
-            )
-        )
-    );
-}
-
-const prompt = ({
-    options,
-    question,
-    title,
-}) =>
-    new Promise((resolve) => {
-        const app = document.getElementById("app");
-        const parent = document.createElement("div");
-        parent.id = 'wiki-prompt-root';
-        app.parentElement.appendChild(parent);
-
-        window.ReactDOM.render(
-            window.React.createElement(
-                FormDialog,
-                {
-                    onSubmit: resolve,
-                    title,
-                    options,
-                    question,
-                    onClose: () => {
-                        window.ReactDOM.unmountComponentAtNode(parent);
-                        parent.remove();
-                    }
-                }
-            ),
-            parent
-        )
-    });
-
 var ARTlanguage = "en";
 var OTDlanguage = "en";
 var FClanguage = "en";
@@ -448,24 +316,24 @@ export default {
                 }
                 var ranNums = shuffle(data.selected);
 
-                const regex = /(\s\((.+)?\s?pictured\)|\s\((.+)?\s?shown\)|\s\((.+)?\s?depicted\))/gm;
+                const regex = /(\s\((.+)?\s?pictured\)|\s\((.+)?\s?shown\)|\s\((.+)?\s?depicted\)|\s\((.+)?\s?audio featured\))/gm;
                 const subst = ``;
                 let string = "in ";
                 string += ranNums[0].year;
                 string += ":\n\n";
-                const result = ranNums[0].text.replace(regex, subst);
+                var result = ranNums[0].text.replace(regex, subst);
                 string += result;
                 string += "";
                 let string1 = "in ";
                 string1 += ranNums[1].year;
                 string1 += ":\n\n";
-                const result1 = ranNums[1].text.replace(regex, subst);
+                var result1 = ranNums[1].text.replace(regex, subst);
                 string1 += result1;
                 string1 += "";
                 let string2 = "in ";
                 string2 += ranNums[2].year;
                 string2 += ":\n\n";
-                const result2 = ranNums[2].text.replace(regex, subst);
+                var result2 = ranNums[2].text.replace(regex, subst);
                 string2 += result2;
                 string2 += "";
                 return [
@@ -559,8 +427,142 @@ export default {
     }
 }
 
+// helper functions
+
 function sendConfigAlert(key) {
     if (key == "sentences") {
         alert("Please enter an integer for extract length in the configuration settings via the Roam Depot tab.");
     }
 }
+
+// copied and adapted from https://github.com/dvargas92495/roamjs-components/blob/main/src/writes/createBlock.ts
+const createBlock = (params) => {
+    const uid = window.roamAlphaAPI.util.generateUID();
+    return Promise.all([
+        window.roamAlphaAPI.createBlock({
+            location: {
+                "parent-uid": params.parentUid,
+                order: params.order,
+            },
+            block: {
+                uid,
+                string: params.node.text
+            }
+        })
+    ].concat((params.node.children || []).map((node, order) =>
+        createBlock({ parentUid: uid, order, node })
+    )))
+};
+
+// copied and adapted from https://github.com/dvargas92495/roamjs-components/blob/main/src/components/FormDialog.tsx
+const FormDialog = ({
+    onSubmit,
+    title,
+    options,
+    question,
+    onClose,
+}) => {
+    const [data, setData] = window.React.useState(options[0].id);
+    const onClick = window.React.useCallback(
+        () => {
+            onSubmit(data);
+            onClose();
+        },
+        [data, onClose]
+    );
+    const onCancel = window.React.useCallback(
+        () => {
+            onSubmit("");
+            onClose();
+        },
+        [onClose]
+    )
+    return window.React.createElement(
+        window.Blueprint.Core.Dialog,
+        { isOpen: true, onClose: onCancel, title, },
+        window.React.createElement(
+            "div",
+            { className: window.Blueprint.Core.Classes.DIALOG_BODY },
+            question,
+            window.React.createElement(
+                window.Blueprint.Core.Label,
+                {},
+                "Wikipedia Entries:",
+                window.React.createElement(
+                    window.Blueprint.Select.Select,
+                    {
+                        activeItem: data,
+                        onItemSelect: (id) => setData(id),
+                        items: options.map(opt => opt.id),
+                        itemRenderer: (item, { modifiers, handleClick }) => window.React.createElement(
+                            window.Blueprint.Core.MenuItem,
+                            {
+                                key: item,
+                                text: options.find(opt => opt.id === item).label,
+                                active: modifiers.active,
+                                onClick: handleClick,
+                            }
+                        ),
+                        filterable: false,
+                        popoverProps: {
+                            minimal: true,
+                            captureDismiss: true,
+                        }
+                    },
+                    window.React.createElement(
+                        window.Blueprint.Core.Button,
+                        {
+                            text: options.find(opt => opt.id === data).label,
+                            rightIcon: "double-caret-vertical"
+                        }
+                    )
+                )
+            )
+        ),
+        window.React.createElement(
+            "div",
+            { className: window.Blueprint.Core.Classes.DIALOG_FOOTER },
+            window.React.createElement(
+                "div",
+                { className: window.Blueprint.Core.Classes.DIALOG_FOOTER_ACTIONS },
+                window.React.createElement(
+                    window.Blueprint.Core.Button,
+                    { text: "Cancel", onClick: onCancel, }
+                ),
+                window.React.createElement(
+                    window.Blueprint.Core.Button,
+                    { text: "Submit", intent: "primary", onClick }
+                )
+            )
+        )
+    );
+}
+
+const prompt = ({
+    options,
+    question,
+    title,
+}) =>
+    new Promise((resolve) => {
+        const app = document.getElementById("app");
+        const parent = document.createElement("div");
+        parent.id = 'wiki-prompt-root';
+        app.parentElement.appendChild(parent);
+
+        window.ReactDOM.render(
+            window.React.createElement(
+                FormDialog,
+                {
+                    onSubmit: resolve,
+                    title,
+                    options,
+                    question,
+                    onClose: () => {
+                        window.ReactDOM.unmountComponentAtNode(parent);
+                        parent.remove();
+                    }
+                }
+            ),
+            parent
+        )
+    });
